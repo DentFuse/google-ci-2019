85ec919a7751053e8d787959e33c64f26b278f96
DentFuse <shivam.dwi253@gmail.com>
2019-12-22 22:40:56 +0530

    Added stuff as requested

--- python-telegram-bot/index.py
+++ python-telegram-bot/index.py
@@ -8,8 +8,7 @@ updater = Updater(token='992019453:AAFcLKDXFXmpAEBhyNJfrup7ExXyEm7uhkI', use_con
 
 def start(update, context):
 	context.bot.send_message(chat_id=update.effective_chat.id, text="Getting the latest data out of the oven for you, please wait!")
-	res = requests.get('https://api.github.com/orgs/fedora-infra/repos')
-	res = json.loads(res.text)
+	res = getApi();
 	totalForks = 0
 	for i in res:
 		# logging.info(i)
@@ -18,8 +17,24 @@ def start(update, context):
 	# logging.info(totalForks);
 	context.bot.send_message(chat_id=update.effective_chat.id, text="Number of forks for all repos of fedora-infra is " + str(totalForks))
 
+def forkCount(update, context):
+	# logging.info(update.message.text);
+	text = update.message.text.split();
+	repo = text[1];
+	res = getApi();
+	for i in res:
+		if(i['name'] == repo):
+			return context.bot.send_message(chat_id=update.effective_chat.id, text="Number of forks for {} is {}".format(repo, str(i['forks'])))
+		# logging.info(i['name'])
+
+
+def getApi():
+	res = requests.get('https://api.github.com/orgs/fedora-infra/repos')
+	res = json.loads(res.text)
+	return res;
+
 dispatcher = updater.dispatcher
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('forks', forkCount))
 updater.start_polling()
 logging.info('Ready!');
